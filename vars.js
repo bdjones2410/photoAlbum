ea597602d0a71234405af79b2e7aacadaaec6b1e
Brandon Jones <bdjones2410@gmail.com>
2015-10-22 16:25:19 -0400

    layout and array created for photoAlbum project

---
+++ vars.js
@@ -0,0 +1,92 @@
+var imageArray =[
+  {
+  album: 1,
+  picURL:"photoImages/buildings1.jpg",
+  picDescript:"Photo of Awesome Building"
+},
+{
+  album: 1,
+  picURL:"photoImages/buildings2.jpg",
+  picDescript:"Photo of second Building"
+},
+{
+  album: 1,
+  picURL: "photoImages/buildings3.jpg",
+  picDescript:"Pic of yet more buildings"
+},
+{
+  album: 2,
+  picURL:"photoImages/flower1.jpg",
+  picDescript:"Glowing Flower"
+},
+{
+  album: 2,
+  picURL:"photoImages/flowers2.jpg",
+  picDescript: "cool flower pic"
+},
+{
+  album: 2,
+  picURL:"photoImages/flowers3.jpg",
+  picDescript: "one more awesome flower"
+},
+{
+  album: 3,
+  picURL: "photoImages/gamenight1.jpg",
+  picDescript:"having a fun Game Night"
+},
+{
+  album: 3,
+  picURL: "photoImages/gamenight2.jpg",
+  picDescript:"More games!"
+},
+{
+  album: 3,
+  picURL: "photoImages/gamenight3.jpg",
+  picDescript: "So many games"
+},
+{
+  album: 4,
+  picURL: "photoImages/khazix_1.jpg",
+  picDescript: "Bug Jungler"
+},
+{
+  album: 4,
+  picURL: "photoImages/kindred.jpg",
+  picDescript: "Lamb and Wolf"
+},
+{
+  album: 4,
+  picURL: "photoImages/Rengar_Splash.jpg",
+  picDescript: "Rengu jump"
+},
+{
+  album: 5,
+  picURL: "photoImages/portrait1.jpg",
+  picDescript: "portrait study"
+},
+{
+  album: 5,
+  picURL: "photoImages/portrait2.jpg",
+  picDescript: "portrait study 2"
+},
+{
+  album: 5,
+  picURL: "photoImages/portrait3.jpg",
+  picDescript: "portrait study 3"
+},
+{
+  album: 6,
+  picURL: "photoImages/sculpture1.jpg",
+  picDescript: "mother nature sculpture"
+},
+{
+  album: 6,
+  picURL: "photoImages/sculpture2.jpg",
+  picDescript: "nature themed sculpture"
+},
+{
+  album: 6,
+  picURL: "photoImages/sculpture3.jpg",
+  picDescript: "more sculptures"
+}
+];
