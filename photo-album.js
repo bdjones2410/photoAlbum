9c5426c1f6e87fe5127f0ff0aabac45fa11560f4
Brandon Jones <bdjones2410@gmail.com>
2015-10-23 19:28:29 -0400

    styled and added images to first two sections. worked on links and templates

--- photo-album.js
+++ photo-album.js
@@ -1,7 +1,142 @@
-$('document').ready(function(){
+$(document).ready(function(){
+
+////////////////////TEMPLATES/////////////////////////////////
+
+
+var galleryTemp = _.template($('#galTemp').html());
+window.galHTML ="";
+
+var titleTemp = _.template($('#titleTemp').html());
+window.titleHTML ="";
+
+var photoTemp = _.template($('#picTemp').html());
+window.photoHTML ="";
+
+_.each(oneOfEach, function(curItem) {
+  photoHTML += photoTemp(curItem);
+});
+
+$('.albumSpace').html(photoHTML);
+
+/////////////////////Gallery Page //////////////////////////////
+
+$('body').on('click','.buildings', function(event){
+  event.preventDefault();
+    $('.galleryDisplay').addClass('activate');
+    $('.albumDisplay').removeClass('activate');
+    titleHTML ="";
+
+    titleHTML +=titleTemp(albumOne[0]);
+    $('.photoDisplayCase').html(titleHTML);
+
+
+      galHTML ="";
+    _.each(albumOne, function(curItem) {
+      galHTML += galleryTemp(curItem);
+    });
+
+    $('.photoDisplayCase').append(galHTML);
+});
+
+$('body').on('click','.flowers', function(event){
+    event.preventDefault();
+      $('.galleryDisplay').addClass('activate');
+      $('.albumDisplay').removeClass('activate');
+      titleHTML ="";
+
+      titleHTML +=titleTemp(albumTwo[0]);
+      $('.photoDisplayCase').html(titleHTML);
+
+
+      galHTML ="";
+    _.each(albumTwo, function(curItem) {
+      galHTML += galleryTemp(curItem);
+    });
+
+    $('.photoDisplayCase').append(galHTML);
+});
+
+$('body').on('click','.gameNight', function(event){
+  event.preventDefault();
+    $('.galleryDisplay').addClass('activate');
+    $('.albumDisplay').removeClass('activate');
+    titleHTML ="";
+
+    titleHTML +=titleTemp(albumThree[0]);
+    $('.photoDisplayCase').html(titleHTML);
+
+    galHTML ="";
+  _.each(albumThree, function(curItem) {
+    galHTML += galleryTemp(curItem);
+  });
+
+  $('.photoDisplayCase').append(galHTML);
+});
+
+
+$('body').on('click','.League', function(event){
+  event.preventDefault();
+    $('.galleryDisplay').addClass('activate');
+    $('.albumDisplay').removeClass('activate');
+    titleHTML ="";
+
+    titleHTML +=titleTemp(albumFour[0]);
+    $('.photoDisplayCase').html(titleHTML);
+
+
+    galHTML ="";
+  _.each(albumFour, function(curItem) {
+    galHTML += galleryTemp(curItem);
+  });
+
+  $('.photoDisplayCase').append(galHTML);
+});
+
+$('body').on('click','.portraits', function(event){
+  event.preventDefault();
+    $('.galleryDisplay').addClass('activate');
+    $('.albumDisplay').removeClass('activate');
+    titleHTML ="";
+
+    titleHTML +=titleTemp(albumFive[0]);
+    $('.photoDisplayCase').html(titleHTML);
+
+
+    galHTML ="";
+  _.each(albumFive, function(curItem) {
+    galHTML += galleryTemp(curItem);
+  });
+
+  $('.photoDisplayCase').append(galHTML);
+});
+
+
+$('body').on('click','.sculptures', function(event){
+  event.preventDefault();
+    $('.galleryDisplay').addClass('activate');
+    $('.albumDisplay').removeClass('activate');
+    titleHTML ="";
+
+    titleHTML +=titleTemp(albumSix[0]);
+    $('.photoDisplayCase').html(titleHTML);
 
 
 
+    galHTML ="";
+  _.each(albumSix, function(curItem) {
+    galHTML += galleryTemp(curItem);
+  });
+
+  $('.photoDisplayCase').append(galHTML);
+});
+
+///////////////////////// Picture View //////////////////////
+
+$('body').on('click','.one', function(event) {
+  event.preventDefault();
+  $('.photoDisplay').addClass('activate');
+  $('.galleryDisplay').removeClass('activate');
+});
 
 
 
--- vars.js
+++ vars.js
@@ -1,92 +1,320 @@
 var imageArray =[
   {
   album: 1,
+  picID: "one",
+  albumNamed: "Buildings",
   picURL:"photoImages/buildings1.jpg",
   picDescript:"Photo of Awesome Building"
 },
 {
   album: 1,
+  picID: "two",
+  albumNamed: "Buildings",
   picURL:"photoImages/buildings2.jpg",
   picDescript:"Photo of second Building"
 },
 {
   album: 1,
+  picID: "three",
+  albumNamed: "Buildings",
   picURL: "photoImages/buildings3.jpg",
   picDescript:"Pic of yet more buildings"
 },
 {
   album: 2,
+  picID: "one",
+  albumNamed: "Flowers",
   picURL:"photoImages/flower1.jpg",
   picDescript:"Glowing Flower"
 },
 {
   album: 2,
+  picID: "two",
+  albumNamed: "Flowers",
   picURL:"photoImages/flowers2.jpg",
   picDescript: "cool flower pic"
 },
 {
   album: 2,
+  picID: "three",
+  albumNamed: "Flowers",
   picURL:"photoImages/flowers3.jpg",
   picDescript: "one more awesome flower"
 },
 {
   album: 3,
+  picID: "one",
+  albumNamed: "Game Night",
   picURL: "photoImages/gamenight1.jpg",
   picDescript:"having a fun Game Night"
 },
 {
   album: 3,
+  picID: "two",
+  albumNamed: "Game Night",
   picURL: "photoImages/gamenight2.jpg",
   picDescript:"More games!"
 },
 {
   album: 3,
+  picID: "three",
+  albumNamed: "Game Night",
   picURL: "photoImages/gamenight3.jpg",
   picDescript: "So many games"
 },
 {
   album: 4,
+  picID: "one",
+  albumNamed: "League",
   picURL: "photoImages/khazix_1.jpg",
   picDescript: "Bug Jungler"
 },
 {
   album: 4,
+  picID: "two",
+  albumNamed: "League",
   picURL: "photoImages/kindred.jpg",
   picDescript: "Lamb and Wolf"
 },
 {
   album: 4,
+  picID: "three",
+  albumNamed: "League",
   picURL: "photoImages/Rengar_Splash.jpg",
   picDescript: "Rengu jump"
 },
 {
   album: 5,
+  picID: "one",
+  albumNamed: "Portraits",
   picURL: "photoImages/portrait1.jpg",
   picDescript: "portrait study"
 },
 {
   album: 5,
+  picID: "two",
+  albumNamed: "Portraits",
   picURL: "photoImages/portrait2.jpg",
   picDescript: "portrait study 2"
 },
 {
   album: 5,
+  picID: "three",
+  albumNamed: "Portraits",
   picURL: "photoImages/portrait3.jpg",
   picDescript: "portrait study 3"
 },
 {
   album: 6,
+  picID: "one",
+  albumNamed: "Sculptures",
   picURL: "photoImages/sculpture1.jpg",
   picDescript: "mother nature sculpture"
 },
 {
   album: 6,
+  picID: "two",
+  albumNamed: "Sculptures",
   picURL: "photoImages/sculpture2.jpg",
   picDescript: "nature themed sculpture"
 },
 {
   album: 6,
+  picID: "three",
+  albumNamed: "Sculptures",
   picURL: "photoImages/sculpture3.jpg",
   picDescript: "more sculptures"
 }
 ];
+
+var oneOfEach = [{
+  album: "buildings",
+  picURL:"photoImages/buildings1.jpg",
+  picDescript:"Building Album"
+},
+{
+  album: "flowers",
+  picURL:"photoImages/flower1.jpg",
+  picDescript:"Flowers Album"
+},
+{
+  album: "gameNight",
+  picURL: "photoImages/gamenight1.jpg",
+  picDescript:"Game Night"
+},
+{
+  album: "League",
+  picURL: "photoImages/khazix_1.jpg",
+  picDescript: "League of Legends"
+},
+{
+  album: "portraits",
+  picURL: "photoImages/portrait3.jpg",
+  picDescript: "portrait studies"
+},
+{
+  album: "sculptures",
+  picURL: "photoImages/sculpture1.jpg",
+  picDescript: "sculptures"
+},
+];
+
+var albumOne = _.filter(imageArray, function(el) {
+  return (el.album===1);
+});
+
+var albumTwo = _.filter(imageArray, function(el) {
+  return (el.album===2);
+});
+
+var albumThree = _.filter(imageArray, function(el) {
+  return (el.album===3);
+});
+
+var albumFour = _.filter(imageArray, function(el) {
+  return (el.album===4);
+});
+
+var albumFive = _.filter(imageArray, function(el) {
+  return (el.album===5);
+});
+var albumSix = _.filter(imageArray, function(el) {
+  return (el.album===6);
+});
+
+
+//
+// var deeperImageArray =[
+//   {
+//     albumName: "Buildings",
+//     albumPics:  [{
+//         album:1,
+//         picURL:"photoImages/buildings1.jpg",
+//         picDescript:"Photo of Awesome Building"
+//         },
+//         {
+//           album: 1,
+//           picURL:"photoImages/buildings2.jpg",
+//           picDescript:"Photo of second Building"
+//         },
+//         {
+//           album: 1,
+//           picURL: "photoImages/buildings3.jpg",
+//           picDescript:"Pic of yet more buildings"
+//         }]
+// },
+//
+// {
+//     albumName: "Flowers",
+//     albumPics: [{
+//
+//             album: 2,
+//             picURL: "photoImages/flower1.jpg",
+//             picDescript: "Glowing Flower"
+//         }, {
+//             album: 2,
+//             picURL: "photoImages/flowers2.jpg",
+//             picDescript: "cool flower pic"
+//         }, {
+//             album: 2,
+//             picURL: "photoImages/flowers3.jpg",
+//             picDescript: "one more awesome flower"
+//         },
+//     ]
+//     },
+// {
+//
+//       albumName: 'Game Night',
+//       albumPics: [{
+//
+//           album: 3,
+//           picURL: "photoImages/gamenight1.jpg",
+//           picDescript:"having a fun Game Night"
+//         },
+//         {
+//           album: 3,
+//           picURL: "photoImages/gamenight2.jpg",
+//           picDescript:"More games!"
+//         },
+//         {
+//           album: 3,
+//           picURL: "photoImages/gamenight3.jpg",
+//           picDescript: "So many games"
+//         }
+//         ]},
+//
+//     {
+//       albumName: 'League of Legends',
+//       albumPics:[
+//
+//         {
+//           album: 4,
+//           picURL: "photoImages/khazix_1.jpg",
+//           picDescript: "Bug Jungler"
+//         },
+//         {
+//           album: 4,
+//           picURL: "photoImages/kindred.jpg",
+//           picDescript: "Lamb and Wolf"
+//         },
+//         {
+//           album: 4,
+//           picURL: "photoImages/Rengar_Splash.jpg",
+//           picDescript: "Rengu jump"
+//         }
+//         ]},
+//
+//         {
+//           albumName: 'Portrait work',
+//           albumPics: [
+//               {
+//                 album: 5,
+//                 picURL: "photoImages/portrait1.jpg",
+//                 picDescript: "portrait study"
+//               },
+//               {
+//                 album: 5,
+//                 picURL: "photoImages/portrait2.jpg",
+//                 picDescript: "portrait study 2"
+//               },
+//               {
+//                 album: 5,
+//                 picURL: "photoImages/portrait3.jpg",
+//                 picDescript: "portrait study 3"
+//               },
+//               ]},
+//
+//           {
+//             albumName: 'Sculptures',
+//             albumPics: [
+//               {
+//             album: 6,
+//             picURL: "photoImages/sculpture1.jpg",
+//             picDescript: "mother nature sculpture"
+//           },
+//           {
+//             album: 6,
+//             picURL: "photoImages/sculpture2.jpg",
+//             picDescript: "nature themed sculpture"
+//           },
+//           {
+//             album: 6,
+//             picURL: "photoImages/sculpture3.jpg",
+//             picDescript: "more sculptures"
+//           }
+//           ]}
+//         ];
+
+//
+// var oneOfEach = _.each(deeperImageArray, function(el){
+//         return(el.albumName + el.albumPics[0].picURL);
+//       }
+// );
+
+
+
+//
+//
+// _.each(deeperImageArray, function(el){ _.each(el.albumPics, function(nel){
+//   console.log(nel.picURL);
+// });});
